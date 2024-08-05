9ed1572e8ed464ce84755d37a5a6365791913959
Поповичев Артём Алексеевич <67004012+ppppppiP@users.noreply.github.com>
2024-08-05 17:49:32 +0300

    сть

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,43 @@
+using Controller;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    [SerializeField] GameObject FinishMenu_Bad;
+    [SerializeField] TextMeshProUGUI _shprizCount;
+    [SerializeField] GameObject FinishMenu_Good;
+    public int MaxShpriz;
+    public int Shpriz;
+
+    public static Finish instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void SetFinishActive()
+    {
+        if (Shpriz < MaxShpriz)
+        {
+            _shprizCount.text = Shpriz.ToString();
+            FinishMenu_Bad.SetActive(true);
+        }
+        else
+        {
+            FinishMenu_Good.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerController>())
+        {
+            SetFinishActive();
+        }
+    }
+}
---
+++ Assets/Scripts/ShpizGetter.cs
@@ -0,0 +1,14 @@
+﻿using Controller;
+using UnityEngine;
+
+public class ShpizGetter: MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            Finish.instance.Shpriz++;
+            gameObject.SetActive(false);
+        }
+    }
+}
